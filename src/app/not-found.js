6f714f6f9630e5d0f3aa672205179816e3078a24
ashrafkurt <eshrefkurt@gmail.com>
2025-06-11 23:07:28 +0300

    .

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,3 +1,8 @@
+export const metadata = {
+  title: '404 - Page Not Found | Moda Çiçekçi',
+  description: 'The page you are looking for does not exist.'
+}
+
 export default function NotFound() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
